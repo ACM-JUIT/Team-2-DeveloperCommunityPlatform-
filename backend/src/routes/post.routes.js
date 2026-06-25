2d47cade28c814e32ee7b08ebaa1d5464ce4add5
Surbhi Kumari <rpsurbhi955@gmail.com>
2026-06-26 00:43:35 +0530

    commit changes

--- backend/src/routes/post.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPost } from "../controllers/post.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
-
-const router = express.Router();
-
-router.post("/", authMiddleware, createPost);
-
-export default router;
